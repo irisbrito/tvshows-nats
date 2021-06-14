51a6ed9af5acbb708c1dee45abb9646107f887ce
Iris <irisbritox@gmail.com>
2021-06-14 16:20:44 -0300

    Adicionar método put na SerieController

--- src/main/kotlin/com/iris/controller/SerieController.kt
+++ src/main/kotlin/com/iris/controller/SerieController.kt
@@ -4,9 +4,7 @@ import com.iris.model.Serie
 import com.iris.service.SerieService
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
-import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.*
 import org.slf4j.LoggerFactory
 import java.util.*
 
@@ -19,6 +17,12 @@ class SerieController(private val serieService: SerieService) {
     fun create(@Body serie: Serie): HttpResponse<Any> {
         serieService.send(Serie(UUID.randomUUID(), serie.name, serie.description, serie.description, serie.whereToWatch))
         return HttpResponse.created(HttpStatus.ACCEPTED).body(this.serieService.send(serie))
+    }
+
+    @Put("/{id}")
+    fun update(@PathVariable uuid: UUID, @Body serie: Serie): HttpResponse<Serie> {
+        serieService.send(Serie(serie.id, serie.name, serie.description, serie.description, serie.whereToWatch))
+        return HttpResponse.ok(HttpStatus.OK).body(this.serieService.send(serie))
     }
 
 }
